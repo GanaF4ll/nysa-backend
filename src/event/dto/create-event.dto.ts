afa50dc3db97266d0782c8355e0da5924fbec712
GanaF4ll <ganafall9498@gmail.com>
2024-12-03 19:44:40 +0100

    🏷️ event dto

--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -1,1 +1,50 @@
-export class CreateEventDto {}
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  IsOptional,
+  IsDateString,
+  Min,
+  Max,
+} from 'class-validator';
+import { Event_visibility } from 'src/constants';
+
+export class CreateEventDto {
+  @IsString()
+  readonly creator_id: string;
+
+  @IsString()
+  readonly title: string;
+
+  @IsDateString()
+  readonly date: string;
+
+  @IsString()
+  readonly address: string;
+
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  readonly latitude: number;
+
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  readonly longitude: number;
+
+  @IsNumber()
+  @Min(1)
+  readonly max_participants: number;
+
+  @IsEnum(Event_visibility)
+  readonly visibility: Event_visibility;
+
+  @IsNumber()
+  readonly entry_fee: number;
+
+  @IsString()
+  readonly description: string;
+
+  @IsNumber()
+  readonly view_count: number;
+}
--- src/event/dto/update-event.dto.ts
+++ src/event/dto/update-event.dto.ts
@@ -1,4 +1,59 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEventDto } from './create-event.dto';
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  IsOptional,
+  IsDateString,
+  Min,
+  Max,
+} from 'class-validator';
+import { Event_visibility } from 'src/constants';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+  @IsOptional()
+  @IsString()
+  readonly title?: string;
+
+  @IsOptional()
+  @IsDateString()
+  readonly date?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly address?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  readonly latitude?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  readonly longitude?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  readonly max_participants?: number;
+
+  @IsOptional()
+  @IsEnum(Event_visibility)
+  readonly visibility?: Event_visibility;
+
+  @IsOptional()
+  @IsNumber()
+  readonly entry_fee?: number;
+
+  @IsOptional()
+  @IsString()
+  readonly description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  readonly view_count?: number;
+}
