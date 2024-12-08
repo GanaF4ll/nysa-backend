f20761c95bf3e951ef0124ecfcce54b83632c84c
GanaF4ll <ganafall9498@gmail.com>
2024-12-08 12:45:44 +0100

    📝 swagger

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -10,37 +10,56 @@ import {
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Crée une ressource Event',
+  })
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventService.create(createEventDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Retourne toutes les ressources Event',
+  })
   findAll() {
     return this.eventService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Retourne une ressource Event',
+  })
   findOne(@Param('id') id: string) {
     return this.eventService.findOne(id);
   }
 
   @Get('creator/:creator_id')
+  @ApiOperation({
+    summary: 'Retourne toutes les ressources Event créées par un utilisateur',
+  })
   findByCreator(@Param('creator_id') creator_id: string) {
     return this.eventService.findByCreator(creator_id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: "Met à jour une ressource Event, nécessite d'être connecté",
+  })
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.eventService.update(id, updateEventDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: "Supprime une ressource Event, nécessite d'être connecté",
+  })
   remove(@Param('id') id: string) {
     return this.eventService.remove(id);
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,28 +13,42 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Crée un utilisateur',
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Récupère tous les utilisateurs',
+  })
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':auth_id')
+  @ApiOperation({
+    summary: 'Récupère un utilisateur',
+  })
   findOne(@Param('auth_id') auth_id: string) {
     return this.userService.findOne(auth_id);
   }
 
   @UseGuards(AuthGuard)
   @Patch()
+  @ApiOperation({
+    summary:
+      "Met à jour un utilisateur, seul l'utilisateur authentifié peut le faire",
+  })
   update(@Req() request: Request, @Body() updateUserDto: UpdateUserDto) {
     const auth_id = request['user'].id;
     return this.userService.update(auth_id, updateUserDto);
@@ -42,13 +56,21 @@ export class UserController {
 
   @UseGuards(AuthGuard)
   @Patch('/deactivate')
+  @ApiOperation({
+    summary:
+      "Désactive un utilisateur, seul l'utilisateur authentifié peut le faire",
+  })
   deactivate(@Req() request: Request) {
     const auth_id = request['user'].id;
     return this.userService.deactivate(auth_id);
   }
 
-  // @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   @Delete(':auth_id')
+  @ApiOperation({
+    summary:
+      "Supprime un utilisateur, seul l'utilisateur authentifié peut le faire",
+  })
   remove(@Req() request: Request) {
     const auth_id = request['user'].id;
     return this.userService.remove(auth_id);
