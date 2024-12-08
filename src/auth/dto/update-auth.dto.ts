83126d999919b518d5131801bb8d8453d1b77b7d
GanaF4ll <ganafall9498@gmail.com>
2024-12-08 12:24:05 +0100

    📝 ➕ swagger doc

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -8,17 +8,34 @@ import {
   IsEnum,
 } from 'class-validator';
 import { Auth_type } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateAuthDto extends PartialType(CreateAuthDto) {
   @IsEmail()
   @IsOptional()
+  @ApiProperty({
+    type: String,
+    description: "L'email de l'utilisateur",
+    example: 'test@gmail.com',
+  })
   readonly email: string;
 
   @IsString()
   @MinLength(8)
   @IsOptional()
+  @ApiProperty({
+    type: String,
+    description: "Le mot de passe de l'utilisateur",
+    example: 'Test!1234',
+  })
   password: string;
 
   @IsEnum(Auth_type, { message: 'type must be either USER or ORGANISATION' })
   @IsOptional()
+  @ApiProperty({
+    enum: Auth_type,
+    description: "Le type de l'utilisateur",
+    example: ['USER', 'ORGANISATION'],
+  })
   type: Auth_type;
 }
--- src/event/dto/create-event.dto.ts
+++ src/event/dto/create-event.dto.ts
@@ -8,45 +8,105 @@ import {
   Max,
 } from 'class-validator';
 import { Event_visibility } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEventDto {
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: "ID du créateur de l'événement",
+    example: 'cm48rr8fv0000ublpox84yv1y',
+  })
   readonly creator_id: string;
 
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: "titre de l'événement",
+    example: 'Soirée Bousilleurs',
+  })
   readonly title: string;
 
   @IsDateString()
+  @ApiProperty({
+    type: String,
+    description:
+      "Date de l'évènement, sous format string, conversion en format date faite dans le service",
+    example: '01/01/2025',
+  })
   readonly date: string;
 
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: "adresse de l'évènement",
+    example: '24 rue des bousilleurs, 75000 Paris',
+  })
   readonly address: string;
 
   @IsNumber()
   @Min(-90)
   @Max(90)
+  @ApiProperty({
+    type: Number,
+    description:
+      "latitude pour géolocalisation de l'évènement, MIN: -90, MAX: 90",
+    example: '48.8566',
+  })
   readonly latitude: number;
 
   @IsNumber()
   @Min(-180)
   @Max(180)
+  @ApiProperty({
+    type: Number,
+    description:
+      "longitude pour géolocalisation de l'évènement, MIN: -180, MAX: 180",
+    example: '2.3522',
+  })
   readonly longitude: number;
 
   @IsNumber()
   @Min(1)
+  @ApiProperty({
+    type: Number,
+    description:
+      "nombre de participants maximum dans l'évènement, MIN: 1, MAX: ?",
+    example: '10',
+  })
   readonly max_participants: number;
 
   @IsEnum(Event_visibility)
+  @ApiProperty({
+    enum: Event_visibility,
+    description: "visibilité de l'évènement",
+    example: ['PUBLIC', 'PRIVATE', 'FRIENDSONLY'],
+  })
   readonly visibility: Event_visibility;
 
   @IsNumber()
   @Min(0)
+  @ApiProperty({
+    type: Number,
+    description: "prix d'entrée de l'évènement, MIN: 0",
+    example: '10',
+  })
   readonly entry_fee: number;
 
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: "description de l'évènement",
+    example: 'Venez nombreux à la soirée Bousilleurs !',
+  })
   readonly description: string;
 
   @IsNumber()
   @Min(0)
+  @ApiProperty({
+    type: Number,
+    description: "nombre de vues de l'évènement",
+    example: '0',
+  })
   readonly view_count: number;
 }
