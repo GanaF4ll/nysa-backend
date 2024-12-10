24efb40eaac635245d30181f4f0a64e9064111a3
GanaF4ll <ganafall9498@gmail.com>
2024-12-10 22:58:23 +0100

    🚧 event member service

--- src/event/member/dto/create-member.dto.ts
+++ src/event/member/dto/create-member.dto.ts
@@ -1,9 +1,6 @@
-import { Member_status } from '@prisma/client';
-import { IsEnum, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateMemberDto {
   @IsString()
   readonly user_id: string;
-  @IsEnum(Member_status)
-  readonly status: Member_status;
 }
