5fd7eef8f869e8610fdfc434640fe5b02b2ac152
GanaF4ll <ganafall9498@gmail.com>
2024-12-10 16:56:51 +0100

    image.service for event_Images

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -3,10 +3,11 @@ import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { PrismaModule } from 'src/db/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { ImageService } from './image/image.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, ImageService],
 })
 export class EventModule {}
