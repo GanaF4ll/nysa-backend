eb01354cda1d2cda545f9b9a5b1891460710ce37
GanaF4ll <ganafall9498@gmail.com>
2025-01-12 01:35:37 +0100

    🏷️ response type interface

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -7,13 +7,16 @@ import {
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { PrismaService } from 'src/db/prisma.service';
-import { UserService } from 'src/user/user.service';
+import { ResponseType } from 'src/interfaces/response-type';
 
 @Injectable()
 export class EventService {
   constructor(private prismaService: PrismaService) {}
 
-  async create(creator_id, createEventDto: CreateEventDto) {
+  async create(
+    creator_id,
+    createEventDto: CreateEventDto,
+  ): Promise<ResponseType> {
     const { date, ...data } = createEventDto;
     const existingUser = await this.prismaService.user.findUnique({
       where: { id: creator_id },
@@ -42,48 +45,57 @@ export class EventService {
     });
 
     if (newEvent) {
-      return 'Event created successfully';
+      return { message: 'Event created successfully', status: 201 };
     }
 
     throw new BadRequestException('Event not created');
   }
 
-  async findAll() {
-    const events = this.prismaService.event.findMany();
+  async findAll(): Promise<ResponseType> {
+    const events = await this.prismaService.event.findMany();
 
     if (!events) {
       throw new NotFoundException('No events found');
     }
 
-    return events;
+    return { message: 'Events found', data: events, status: 200 };
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<ResponseType> {
     const event = this.prismaService.event.findUnique({ where: { id } });
 
     if (!event) {
       throw new NotFoundException('Event not found');
     }
-    return event;
+    return { message: 'Event found', data: event, status: 200 };
   }
 
-  async findByCreator(creator_id: string) {
+  async findByCreator(creator_id: string): Promise<ResponseType> {
     const creator = await this.prismaService.user.findUnique({
       where: { id: creator_id },
     });
 
-    const events = this.prismaService.event.findMany({ where: { creator_id } });
+    if (!creator) {
+      throw new NotFoundException(`No user found with id ${creator_id}`);
+    }
 
-    if (!events) {
+    const events = await this.prismaService.event.findMany({
+      where: { creator_id },
+    });
+
+    if (!events || events.length === 0) {
       throw new NotFoundException(
         `No events found for the user with id ${creator_id}`,
       );
     }
 
-    return events;
+    return { message: 'Events found', data: events, status: 200 };
   }
 
-  async update(id: string, updateEventDto: UpdateEventDto) {
+  async update(
+    id: string,
+    updateEventDto: UpdateEventDto,
+  ): Promise<ResponseType> {
     const existingEvent = await this.findOne(id);
 
     if (updateEventDto.date) {
@@ -100,7 +112,11 @@ export class EventService {
     });
 
     if (updatedEvent) {
-      return 'Event updated successfully';
+      return {
+        message: 'Event updated successfully',
+        data: updatedEvent,
+        status: 200,
+      };
     } else {
       throw new BadRequestException('Event not updated');
     }
@@ -117,11 +133,11 @@ export class EventService {
   //   return disabledEvent;
   // }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<ResponseType> {
     const existingEvent = await this.findOne(id);
 
     await this.prismaService.event.delete({ where: { id } });
 
-    return `Event with id ${id} has been deleted`;
+    return { message: `Event with id ${id} has been deleted` };
   }
 }
---
+++ src/interfaces/response-type.ts
@@ -0,0 +1,7 @@
+export interface ResponseType {
+  message: string;
+  data?: any;
+  status?: number;
+}
+
+// TODO: implement the response interface on all services
