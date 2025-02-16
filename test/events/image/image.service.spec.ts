da223211c96d94b2f8b1519345dda003e572e109
GanaF4ll <ganafall9498@gmail.com>
2025-02-16 20:47:14 +0100

    🧪

--- test/events/image/image.service.spec.ts
+++ test/events/image/image.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ImageService } from '../../src/events/image/image.service';
+import { ImageService } from 'src/events/image/image.service';
 
 describe('ImageService', () => {
   let service: ImageService;
--- test/stripe/stripe.service.spec.ts
+++ test/stripe/stripe.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { StripeService } from '../../src/stripe/stripe.service';
+import { StripeService } from 'src/stripe/stripe.service';
 
 describe('StripeService', () => {
   let service: StripeService;
